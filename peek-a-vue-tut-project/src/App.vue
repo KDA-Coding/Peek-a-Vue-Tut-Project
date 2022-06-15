46d2526668380a9f8217f7281cb540d0fd79f3e8
Kirk Allgeyer <59184838+KDA-Coding@users.noreply.github.com>
2022-06-15 10:24:44 -0400

    Initial Game Board Complete
    Created visual template for game board and styled accordingly.

--- peek-a-vue-tut-project/src/App.vue
+++ peek-a-vue-tut-project/src/App.vue
@@ -1,5 +1,23 @@
 <template>
   <h1>Peek-a-Vue</h1>
+  <section class="game-board">
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+  </section>
 </template>
 
 <script>
@@ -15,5 +33,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.card {
+  border: 5px solid #ccc;
+}
+
+.game-board {
+  justify-content: center;
+  display: grid;
+  grid-template-columns: 100px 100px 100px 100px;
+  grid-template-rows: 100px 100px 100px 100px;
+  grid-column-gap: 30px;
+  grid-row-gap: 30px;
 }
 </style>

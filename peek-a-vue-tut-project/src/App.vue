a0076385fc116f8346ab6c8c92579890bcf06f1d
Kirk Allgeyer <59184838+KDA-Coding@users.noreply.github.com>
2022-06-16 09:54:33 -0400

    Added Restart Game and fixed computed prop errors
    Added functionality to shuffle the cards each time the game restarts.
    
    Also removed instances where status was being set since it is now a computed property

--- peek-a-vue-tut-project/src/App.vue
+++ peek-a-vue-tut-project/src/App.vue
@@ -10,7 +10,7 @@
     @select-card="flipCard" />
   </section>
   <h2>{{ status }}</h2>
-  <button @click="shuffleCards">Shuffle Cards</button>
+  <button @click="restartGame">Restart Game</button>
 </template>
 
 <script>
@@ -50,10 +50,23 @@ export default {
       cardList.value = _.shuffle(cardList.value)
     }
 
+    const restartGame = () => {
+      shuffleCards()
+
+      cardList.value = cardList.value.map((card, index) => {
+        return {
+          ...card,
+          matched: false,
+          position: index,
+          visible: false
+        }
+      })
+    }
+
     for (let i = 0; i<16; i++) {
       cardList.value.push({
-        value: i,
-        visible: true,
+        value: 8,
+        visible: false,
         position: i,
         matched: false
       })
@@ -77,7 +90,6 @@ export default {
         const cardTwo = currentValue[1]
 
         if(cardOne.faceValue === cardTwo.faceValue) {
-          status.value = 'Matched!'
 
           cardList.value[cardOne.position].matched = true;
           cardList.value[cardTwo.position].matched = true;
@@ -86,7 +98,6 @@ export default {
 
         }
         else {
-          status.value = 'Mismatched.'
 
           cardList.value[cardOne.position].visible = false 
           cardList.value[cardTwo.position].visible = false 
@@ -98,7 +109,8 @@ export default {
       }
     }, {deep: true})
 
-    return { cardList, flipCard, userSelection, status, shuffleCards }
+    return { cardList, userSelection, status, 
+            flipCard, shuffleCards, restartGame }
   }
   
 }
